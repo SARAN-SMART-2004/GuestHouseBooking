fd6378e36410b4d7dcc285ec5df196dabba9ce1f
Saran <119552582+SARAN-SMART-2004@users.noreply.github.com>
2025-06-19 17:49:05 +0530

    yes

--- gbooking/settings.py
+++ gbooking/settings.py
@@ -1,5 +1,5 @@
 
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -14,10 +14,10 @@ SECRET_KEY = "django-insecure-h1=w#g03f^@+_=zmu0@-q9z!&d)))++k)6ygp-2#d@h$m$_utm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -129,4 +129,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Redirect unauthenticated users to the custom login page
 LOGIN_URL = '/auth/login/'
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
